c72b24d0dec767f314a5ab67e806863bbe99cbab
souldiallo <souldiallo@localhost>
2026-08-16 15:58:30 -0400

    Initial commit

--- variable.tf
+++ variable.tf
@@ -1,8 +1,11 @@
-variable "ami-id" {
+variable "ami_id" {
+  type        = string
   default     = "ami-0b6d9d3d33ba97d99"
   description = "The AMI ID for the ALB instances"
 }
-variable "instance-type" {
+
+variable "instance_type" {
+  type        = string
   default     = "t3.micro"
   description = "The instance type for the ALB instances"
 }
