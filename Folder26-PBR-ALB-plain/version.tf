8519e280485cbac94b5a628aa6173493f50805b8
souldiall <souldogomet72@gmail.com>
2026-08-16 20:46:13 -0400

    Uncomment Terraform required_version and providers

--- Folder26-PBR-ALB-plain/version.tf
+++ Folder26-PBR-ALB-plain/version.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   required_version = ">=1.15"
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 6.5"
-#     }
-#   }
-# }
+ terraform {
+   required_version = ">=1.15"
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> 6.5"
+     }
+   }
+ }
